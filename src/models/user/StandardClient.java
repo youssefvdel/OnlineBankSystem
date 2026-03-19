48dbbf372c887ff29dce9e6cacbdec8723fe687c
you864 <youssef.98.ia@gmail.com>
2026-03-19 17:20:39 +0200

    comments

--- src/models/user/StandardClient.java
+++ src/models/user/StandardClient.java
@@ -1,16 +1,42 @@
 package models.user;
 
-public class StandardClient extends Client implements Insurable  {
+/**
+ * @author [yousif hafez_ 258612]
+ * @see Client
+ * @see Insurable
+ * @since phase1
+ */
+public class StandardClient extends Client implements Insurable {
+
     private double withdrawalLimit;
 
+    /**
+     * returns the withdrawal limit of the standard client
+     * @return withdrawalLimit as double
+     */
     public double getWithdrawalLimit() {
         return withdrawalLimit;
     }
 
+    /**
+     * sets the withdrawal limit of the standard client
+     * @param withdrawalLimit as double
+     */
     public void setWithdrawalLimit(double withdrawalLimit) {
         this.withdrawalLimit = withdrawalLimit;
     }
 
+    /**
+     * creates a standard client with all required client information
+     * and assigns the withdrawal limit
+     * @param userId unique user id
+     * @param name client name
+     * @param password client password
+     * @param email client email
+     * @param client_ID client id
+     * @param phoneNumber client phone number
+     * @param withdrawalLimit maximum withdrawal limit
+     */
     public StandardClient(
             String userId,
             String name,
@@ -19,22 +45,42 @@ public class StandardClient extends Client implements Insurable  {
             String client_ID,
             String phoneNumber,
             double withdrawalLimit) {
-        super(userId,name,password,email,client_ID,phoneNumber,"Active");
+        super(userId, name, password, email, client_ID, phoneNumber, "Active");
         this.withdrawalLimit = withdrawalLimit;
     }
 
+    /**
+     * returns the benefits of the standard client
+     * @return benefits as string
+     */
     @Override
     public String getBenefits() {
         return "Withdrawal Limit: " + withdrawalLimit;
     }
 
+    /**
+     * returns the insurance type for the standard client
+     * @return insurance as string
+     */
     @Override
     public String getInsurance() {
         return "Basic account insurance";
     }
 
+    /**
+     * allows the standard client to claim insurance
+     */
     @Override
     public void claimInsurance() {
         System.out.println("Claiming basic insurance...");
+    }
+
+    /**
+     * returns the type of the user
+     * @return user type as string
+     */
+    @Override
+    public String getUserType() {
+        return "Standard Client";
     }
 }
