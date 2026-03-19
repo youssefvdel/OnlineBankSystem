1202128941211c496a227917e77d03bd87dda1da
YousefMohiey <yousefmohiey@gmail.com>
2026-03-19 02:55:51 +0200

    User in progress

--- src/models/user/User.java
+++ src/models/user/User.java
@@ -1,3 +1,41 @@
+/**
+ * User.java
+ * This is an abstract class that represents a user in the banking system.
+ * It is the parent class for Admin and Client.
+ *
+ * @author Yousef Mohiey - 248679
+ * @since Phase 1
+ */
+
 package models.user;
 
-public class User {}
+public abstract class User {
+    // "private" means only User can access those variables
+    private String userId;
+    private String name;
+    private String password;
+    private String email;
+
+    /**
+     * @param userId   The unique ID for this user
+     * @param name     The full name of the user
+     * @param password The password for login confirmation
+     * @param email    The email address of the user
+     */
+    public User(String userId, String name, String password, String email) {
+        this.userId = userId;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+/**
+ * Checks if password is correct
+ * @param inputPassword password user typed
+ * @return true if match, false if wrong
+ */
+public boolean login(String inputPassword)
+{
+    return this.password.equals(inputPassword);
+}
+
+}
